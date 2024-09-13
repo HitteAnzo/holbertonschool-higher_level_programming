bb0058f27dd1b46525814cb5fb3bc8a4737dfd77
HitteAnzols <hitteanzo@gmail.com>
2024-09-13 22:01:51 +0200

    fix 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -4,13 +4,12 @@ This module contains the function text_indentation which prints text
 with two new lines after specific punctuation marks ('.', '?', ':').
 """
 
+
 def text_indentation(text):
     """
     Prints a string with two new lines after each '.', '?' and ':'.
-    
     Args:
         text (str): The string to be processed.
-    
     Raises:
         TypeError: If the input text is not a string.
     """
@@ -18,13 +17,13 @@ def text_indentation(text):
         raise TypeError("text must be a string")
 
     text = text.strip()
-    
+
     i = 0
     while i < len(text):
         print(text[i], end="")
         if text[i] in ['.', '?', ':']:
             print("\n")
-            
+
             i += 1
             while i < len(text) and text[i] == ' ':
                 i += 1
