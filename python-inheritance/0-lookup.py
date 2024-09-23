4098c41f248b767653451c73b97991f43f8690ac
HitteAnzo <hitteanzo@gmail.com>
2024-09-23 10:21:24 +0200

    add 0 + 1 + test 1

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Return the list of available attributes.
+"""
+ 
+
+def lookup(obj):
+    """
+    Args:
+        obj (any): The object to inspect.
+
+    Returns:
+        list: A list of strings representing the object's
+    """
+
+    return dir(obj)
---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module 1-my_list
+Defines a class MyList that inherits from list.
+"""
+
+
+class MyList(list):
+    """
+    A subclass of list that has a method to print a sorted version of itself.
+    """
+
+    def print_sorted(self):
+        """
+        Prints the list in ascending order.
+        """
+
+        print(sorted(self))
