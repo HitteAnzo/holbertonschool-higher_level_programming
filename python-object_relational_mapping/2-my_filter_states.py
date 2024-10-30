f8aad57518e49b397a1fff1a8f2c7ac636b7214c
HitteAnzo <hitteanzo@gmail.com>
2024-10-30 13:01:19 +0100

    fix 2 add 3

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -13,20 +13,17 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
+        post=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
+        db=sys.argv[3]
     )
-
     cursor = db.cursor()
-    
-    # Create the SQL query using format
-    cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4]))
-    
-    rows = cursor.fetchall()
-    for row in rows:
+    query = ("SELECT * FROM states WHERE name"
+             " LIKE '{}' ORDER "
+             "BY id ASC;".format(sys.argv[4]))
+    cursor.execute(query)
+    query_rows = cursor.fetchall()
+    for row in query_rows:
         print(row)
-    
     cursor.close()
-    db.close()
---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""takes in an argument and displays all values in the states table
+but injection safe"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        post=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    query = ("SELECT * FROM states WHERE name"
+             " LIKE '{}' ORDER "
+             "BY id ASC;".format(sys.argv[4]))
+    cursor.execute(query)
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        print(row)
+    cursor.close()
