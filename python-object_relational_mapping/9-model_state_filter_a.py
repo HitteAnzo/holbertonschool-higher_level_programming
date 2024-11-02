2e851ce0e1d024c630dcd0aa01521a12417678f7
HitteAnzols <hitteanzo@gmail.com>
2024-11-02 11:41:28 +0100

    add 9

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
+"""
+Lists all State objects that contain the letter 'a'
+from the database hbtn_0e_6_usa.
+"""
+
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-"""
-Script that lists all State objects that contain the letter 'a'
-from the database hbtn_0e_6_usa.
-"""
-
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}"
-        .format(username, password, database))
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
+    engine = create_engine(
+        f'mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/'
+        f'{database_name}'
+        )
     Session = sessionmaker(bind=engine)
     session = Session()
-    states_with_a = session.query(State).filter(
-        State.name.like('%a%')).order_by(State.id).all()
+    states = session.query(State).filter(State.name.like('%a%'))\
+        .order_by(State.id).all()
 
-    for state in states_with_a:
+    for state in states:
         print(f"{state.id}: {state.name}")
 
     session.close()
